68d8b7dc230cf76e076f6c39675de86a69b5df02
Kalpita Mandal <kalpita103@gmail.com>
2024-06-15 00:28:19 +0530

    Adding public and private input support for avail prover

--- src/routes/Prover/Prover.ts
+++ src/routes/Prover/Prover.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getVersion,
   proveTransaction,
+  proveTransactionPrivate,
 } from "../../controllers/proverControllers/proverController";
 import { validateApiSecret } from "../../middleware/authHelper";
 
@@ -10,5 +11,8 @@ export const prover_router = express.Router();
 //Version check
 prover_router.get("/version", validateApiSecret, getVersion);
 
-//Prove Transaction
+//Prove Transaction public
 prover_router.post("/proveTx", validateApiSecret, proveTransaction);
+
+//Prove Transaction private
+prover_router.post("/proveTxPrivate", validateApiSecret, proveTransactionPrivate);
