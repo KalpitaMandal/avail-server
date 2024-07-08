6f5f86374e4f8dee6f2147941e41f9a0312541a4
Kalpita Mandal <kalpita103@gmail.com>
2024-07-08 13:58:29 +0530

    Updating for authorization and execution

--- src/routes/Prover/Prover.ts
+++ src/routes/Prover/Prover.ts
@@ -2,7 +2,6 @@ import express from "express";
 import {
   getVersion,
   proveTransaction,
-  proveTransactionPrivate,
 } from "../../controllers/proverControllers/proverController";
 import { validateApiSecret } from "../../middleware/authHelper";
 
@@ -14,5 +13,3 @@ prover_router.get("/version", validateApiSecret, getVersion);
 //Prove Transaction public
 prover_router.post("/proveTx", validateApiSecret, proveTransaction);
 
-//Prove Transaction private
-prover_router.post("/proveTxPrivate", validateApiSecret, proveTransactionPrivate);
